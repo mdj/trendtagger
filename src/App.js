a5f826e33830c6ff5de45da6622c25c543988637
Morten Dam Jørgensen <mdj@mdj.dk>
2020-03-30 22:13:39 +0200

    Victory mockups

--- src/App.js
+++ src/App.js
@@ -1,268 +1,112 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-import ReactDOM from 'react-dom';
-import * as V from 'victory';
+// import ReactDOM from 'react-dom';
+// import * as V from 'victory';
 
 import BS from './BS.js';
 
 import Alert from 'react-bootstrap/Alert';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
+// import Form from 'react-bootstrap/Form';
+// import FormControl from 'react-bootstrap/FormControl';
 
+import {csv, timeParse} from 'd3';
 
 function Example() {
-  return (
-    <Alert dismissible variant="danger">
-      <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-      <p>
-        Change this and that and try again.
-      </p>
-    </Alert>
-  )
+    return (
+        <Alert dismissible variant="danger">
+            <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+            <p>
+                Change this and that and try again.
+            </p>
+        </Alert>
+    )
 }
 
 
-const  sampleDataDates = [
-    {
-      x: new Date(2016, 6, 1),
-      open: 5,
-      close: 10,
-      high: 15,
-      low: 0
-    },
-    {
-      x: new Date(2016, 6, 2),
-      open: 10,
-      close: 15,
-      high: 20,
-      low: 5
-    },
-    {
-      x: new Date(2016, 6, 3),
-      open: 15,
-      close: 20,
-      high: 22,
-      low: 10
-    },
-    {
-      x: new Date(2016, 6, 4),
-      open: 20,
-      close: 10,
-      high: 25,
-      low: 7
-    },
-    {
-      x: new Date(2016, 6, 5),
-      open: 10,
-      close: 8,
-      high: 15,
-      low: 5
-    }
-  ];
-
-
 function App() {
+    const [data, setData] = useState([]);
+    const [dimensions, setDimensions] = useState([]);
+    useEffect(() => {
+        csv('ABEV.csv').then(data => {
+            // console.log(data);
+            var parseDate = timeParse('%Y-%m-%dT%H:%M:%S%Z');
+            // console.log(data[2].Date, parseDate(data[2].Date));
 
-  return (
-    <div className="App">
-<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed={"top"}>
-  <Navbar.Brand href="#home">TrendTagger</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#features">Projects</Nav.Link>
-      <Nav.Link href="#pricing">Datasets</Nav.Link>
-      <Nav.Link href="#pricing">Members</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-      <Container fluid>
-        <Row>
-          <Col className="d-none d-md-block bg-light sidebar" md={2}>
-            <div className="sidebar-sticky">
-               <Nav.Link href="#features">Features</Nav.Link>
-            </div>
-          </Col>
-        <Col role={"main"} md={9} sm={"auto"} lg={10}>
-            <div className={"d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"}>
-              <h1 className="h2">Trend series</h1>
-            </div>
+            data.forEach(function (d) {
+                for (var key in d) {
+                    if (!Number.isNaN(Number(d[key]))) {
+                        d[key] = +d[key];
+                    }
+                    // console.log( key, d[key] );
+
+                }
+                d.Date = parseDate(d.Date);
+            });
+            // console.log(data[0])
 
 
-           <V.VictoryChart
-  theme={V.VictoryTheme.material}
-  domainPadding={{ x: 25 }}
-  scale={{ x: "time" }}
->
-<V.VictoryAxis tickFormat={(t) => `${t.getDate()}/${t.getMonth()}`}/>
-<V.VictoryAxis dependentAxis/>
-<V.VictoryCandlestick
-  candleColors={{ positive: "#5f5c5b", negative: "#c43a31" }}
-  data={sampleDataDates}
-/>
-</V.VictoryChart>
+            setData(data);
+            setDimensions(["Volume","Adx14","Mdi14","Pdi14","Adx5","Mdi5","Pdi5","Adx2","Mdi2","Pdi2","PPO12_26","PPO12_26_Signal","Inv_PPO26_12","Inv_PPO26_12_Signal","FastSTOK5","FastSTOD3","FullSTOK3","FullSTOD3","CCI20","CCI15","CCI7","RSI14","RSI7","SMA7","SMA10","SMA20","SMA50","TRIX2","TRIX2_Signal","WILLR14","WILLR7","SAR"]);
+
+        });
+    }, []);
+    return (
+        <div className="App">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed={"top"}>
+                <Navbar.Brand href="#home">TrendTagger</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#features">Projects</Nav.Link>
+                        <Nav.Link href="#pricing">Datasets</Nav.Link>
+                        <Nav.Link href="#pricing">Members</Nav.Link>
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider/>
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Container fluid>
+                <Row>
+                    <Col className="d-none d-md-block bg-light sidebar" md={2}>
+                        <div className="sidebar-sticky">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                        </div>
+                    </Col>
+                    <Col role={"main"} md={9} sm={"auto"} lg={10}>
+                        <div
+                            className={"d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"}>
+                            <h1 className="h2">Trend series</h1>
+                        </div>
+
+                        <BS data={data} dimensions={dimensions}/>
+
+                    </Col>
+                </Row>
 
 
-          <BS/>
-          <h2>Section title</h2>
-          <div className="table-responsive">
-            <table className="table table-striped table-sm">
-              <thead>
-              <tr>
-                <th>#</th>
-                <th>Header</th>
-                <th>Header</th>
-                <th>Header</th>
-                <th>Header</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td>1,001</td>
-                <td>Lorem</td>
-                <td>ipsum</td>
-                <td>dolor</td>
-                <td>sit</td>
-              </tr>
-              <tr>
-                <td>1,002</td>
-                <td>amet</td>
-                <td>consectetur</td>
-                <td>adipiscing</td>
-                <td>elit</td>
-              </tr>
-              <tr>
-                <td>1,003</td>
-                <td>Integer</td>
-                <td>nec</td>
-                <td>odio</td>
-                <td>Praesent</td>
-              </tr>
-              <tr>
-                <td>1,003</td>
-                <td>libero</td>
-                <td>Sed</td>
-                <td>cursus</td>
-                <td>ante</td>
-              </tr>
-              <tr>
-                <td>1,004</td>
-                <td>dapibus</td>
-                <td>diam</td>
-                <td>Sed</td>
-                <td>nisi</td>
-              </tr>
-              <tr>
-                <td>1,005</td>
-                <td>Nulla</td>
-                <td>quis</td>
-                <td>sem</td>
-                <td>at</td>
-              </tr>
-              <tr>
-                <td>1,006</td>
-                <td>nibh</td>
-                <td>elementum</td>
-                <td>imperdiet</td>
-                <td>Duis</td>
-              </tr>
-              <tr>
-                <td>1,007</td>
-                <td>sagittis</td>
-                <td>ipsum</td>
-                <td>Praesent</td>
-                <td>mauris</td>
-              </tr>
-              <tr>
-                <td>1,008</td>
-                <td>Fusce</td>
-                <td>nec</td>
-                <td>tellus</td>
-                <td>sed</td>
-              </tr>
-              <tr>
-                <td>1,009</td>
-                <td>augue</td>
-                <td>semper</td>
-                <td>porta</td>
-                <td>Mauris</td>
-              </tr>
-              <tr>
-                <td>1,010</td>
-                <td>massa</td>
-                <td>Vestibulum</td>
-                <td>lacinia</td>
-                <td>arcu</td>
-              </tr>
-              <tr>
-                <td>1,011</td>
-                <td>eget</td>
-                <td>nulla</td>
-                <td>Class</td>
-                <td>aptent</td>
-              </tr>
-              <tr>
-                <td>1,012</td>
-                <td>taciti</td>
-                <td>sociosqu</td>
-                <td>ad</td>
-                <td>litora</td>
-              </tr>
-              <tr>
-                <td>1,013</td>
-                <td>torquent</td>
-                <td>per</td>
-                <td>conubia</td>
-                <td>nostra</td>
-              </tr>
-              <tr>
-                <td>1,014</td>
-                <td>per</td>
-                <td>inceptos</td>
-                <td>himenaeos</td>
-                <td>Curabitur</td>
-              </tr>
-              <tr>
-                <td>1,015</td>
-                <td>sodales</td>
-                <td>ligula</td>
-                <td>in</td>
-                <td>libero</td>
-              </tr>
-              </tbody>
-            </table>
+            </Container>
 
-          </div>
-
-        </Col>
-        </Row>
-
-
-</Container>
-
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
